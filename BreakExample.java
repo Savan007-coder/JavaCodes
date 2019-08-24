d9bf214ed93ee2919f534fc8d340010136723e4f
Savan007-coder <54475394+Savan007-coder@users.noreply.github.com>
2019-08-24 19:18:05 +0530

    Add files via upload

---
+++ BreakExample.java
@@ -0,0 +1,16 @@
+public class BreakExample
+{
+    public static void main(String[] args)
+    {
+        int i;
+        for(i=1;i<=10;i++)
+        {
+            if(i==6)
+            {
+                System.out.println(i);
+                break;
+            }
+        }
+       
+    }
+}
